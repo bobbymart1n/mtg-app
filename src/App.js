fcacb243b5ca81577914f7b5e27cbcd2d0103ba4
Bobby Martin <robert.martin.619@gmail.com>
2018-10-20 14:03:52 -0700

    Add route for sets

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import CardsPage from './components/CardsPage';
+import Sets from './components/Sets';
 import Navigation from './components/Navigation';
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
         <Switch>
           <Route exact path='/' component={Home} />
           <Route path='/cards' component={CardsPage} />
+          <Route path='/sets' component={Sets} />
         </Switch>
       </div>
     );
