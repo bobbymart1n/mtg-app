8aaec6cc542a1661dc63a946b0aeee3398b88921
Bobby Martin <robert.martin.619@gmail.com>
2018-10-20 14:03:37 -0700

    Add working GET request for set

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -7,6 +7,11 @@ class Cards extends Container<CardState> {
     const data = await response.json();
     this.setState({...data});
   }
+  handleGettingSet = async (set) => {
+    const response = await fetch(`https://api.magicthegathering.io/v1/sets/${set}`);
+    const data = await response.json();
+    console.log(data)
+  }
 }
 
 export default Cards;
