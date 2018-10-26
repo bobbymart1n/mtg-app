3ad5ada5f1b34c1da5e18edf9f40137f87bd934b
Bobby Martin <robert.martin.619@gmail.com>
2018-10-26 11:50:39 -0700

    Add set specific call

--- src/components/Sets.jsx
+++ src/components/Sets.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import { Subscribe } from 'unstated';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 import Cards from './Cards';
 
 const Sets = () => (
-  <Subscribe to={[Cards]}>
-    {sets => (
-      <button type='button' onClick={() => sets.handleGettingSet('xln')}>Ixalan</button>
-    )}
-  </Subscribe>
+  <div>
+  <h1>Set Booster Pack Generator</h1>
+    <Subscribe to={[Cards]}>
+      {sets => (
+        <div>
+          <button type='button' onClick={() => sets.handleGettingSet('xln')}>Ixalan</button>
+          <CardsContainer>
+            {sets.state.cards && (
+              sets.state.cards.map(({id, name, imageUrl}) =>
+                <img src={`${imageUrl}`} alt={name} key={id} />
+              )
+            )}
+          </CardsContainer>
+        </div>
+      )}
+    </Subscribe>
+  </div>
 )
+
+const CardsContainer = styled.div `
+  display: flex;
+  flex-flow: row wrap;
+  margin: auto;
+`
 
 export default Sets
