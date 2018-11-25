3d09f9888ca402a0547c97b72835b0fbed312632
Bobby Martin <robert.martin.619@gmail.com>
2018-11-24 18:27:27 -0800

    Add events and subscribe to it

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
+import { Subscribe } from 'unstated';
+import styled from 'styled-components';
+import EventCard from './EventCard';
+import Events from './../containers/Events';
+import MTGIcon from './../assets/mtg_icon.png';
+
+const Container = styled.section `
+  padding: 30px;
+  width: 100%;
+  height: 100%;
+`
+
 const Home = () => (
-  <div>
-    <h1>Home</h1>
-  </div>
+  <Container>
+    <Subscribe to={[Events]}>
+      {event => (
+        <div>
+          {event.state.events.map(event => (
+            <EventCard
+              title={event.title}
+              date={event.date}
+              shopName={event.shopName}
+              cost={event.cost}
+              icon={MTGIcon} />
+          ))}
+        </div>
+      )}
+    </Subscribe>
+  </Container>
 )
 
 export default Home;
